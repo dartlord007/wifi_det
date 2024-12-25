b6465d6d08a2453378487c26e75191aef37e8ff3
VictorModupe <Victormodupe007@gmail.com>
2024-12-25 16:12:14 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/wifi_det/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.wifi_det
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'dart:async';
+
+// import 'package:wifi_det/connectivity.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:wifi_det/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:overlay_support/overlay_support.dart';
+
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Has Internet?';
+
+  @override
+  Widget build(BuildContext context) => OverlaySupport(
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: title,
+          theme: ThemeData(primarySwatch: Colors.teal),
+          home: MainPage(title: title),
+        ),
+      );
+}
+
+class MainPage extends StatefulWidget {
+  final String title;
+
+  const MainPage({
+    required this.title,
+  });
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late StreamSubscription subscription;
+
+  @override
+  void initState() {
+    super.initState();
+
+    subscription =
+        Connectivity().onConnectivityChanged.listen(showConnectivitySnackBar as void Function(List<ConnectivityResult> event)?);
+  }
+
+  @override
+  void dispose() {
+    subscription.cancel();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(padding: EdgeInsets.all(12)),
+            child: Text('Check Connection', style: TextStyle(fontSize: 20)),
+            onPressed: () async {
+              final result = await Connectivity().checkConnectivity();
+              showConnectivitySnackBar(result as ConnectivityResult);
+            },
+          ),
+        ),
+      );
+
+  void showConnectivitySnackBar(ConnectivityResult result) {
+    final hasInternet = result != ConnectivityResult.none;
+    final message = hasInternet
+        ? 'You have again ${result.toString()}'
+        : 'You have no internet';
+    final color = hasInternet ? Colors.green : Colors.red;
+
+    Utils.showTopSnackBar(context, message, color);
+  }
+}
